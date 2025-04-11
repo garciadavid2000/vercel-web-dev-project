da0c9f5ab91b232b6709a9639b7b5a67d56ed66f
David Garcia <49959037+garciadavid2000@users.noreply.github.com>
2025-04-10 23:18:11 -0400

    headers

--- csci3230u-final-project/src/services/SpotifyDataService.js
+++ csci3230u-final-project/src/services/SpotifyDataService.js
@@ -8,12 +8,12 @@ const apiClient = axios.create({
   // This is useful for cross-origin requests where credentials are not needed.
   withCredentials: true,
   // headers are set to accept and send JSON data
-  headers: {
-    // The 'Accept' header indicates the type of content the client can process.
-    Accept: 'application/json',
-    // The 'Content-Type' header indicates the type of content being sent to the server.
-    'Content-Type': 'application/json',
-  }
+  // headers: {
+  //   // The 'Accept' header indicates the type of content the client can process.
+  //   Accept: 'application/json',
+  //   // The 'Content-Type' header indicates the type of content being sent to the server.
+  //   'Content-Type': 'application/json',
+  // }
 })
 
 // Main value that will be exported when this file is imported elsewhere.
